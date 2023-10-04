1a169cebe131cd582e1bb6645dae6f76aa19841a
Cyan Villarin <cyanvillarin@gmail.com>
2023-10-04 11:48:51 +0900

    🚀 feature - initial files

---
+++ github-users/github_usersApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_usersApp.swift
+//  github-users
+//
+//  Created by CyanCamit.Villari on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct github_usersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ github-usersUITests/github_usersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  github_usersUITestsLaunchTests.swift
+//  github-usersUITests
+//
+//  Created by CyanCamit.Villari on 2023/10/04.
+//
+
+import XCTest
+
+final class github_usersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
